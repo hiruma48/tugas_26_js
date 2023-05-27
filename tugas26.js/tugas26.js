590caa57825503d34b46e0c885e85a2162305b78
tri ariyanto <triyanto129@gmail.com>
2023-05-27 10:43:03 +0700

    tugas 26 js
    mengumpulkan tugas

---
+++ tugas26.js/tugas26.js
@@ -0,0 +1,19 @@
+function div(x) {
+  if (isFinite(x)) {
+    return 'NOT Infinity.';
+  }
+  return 'Infinity!';
+}
+
+console.log("Angka 2 ",div(2));
+console.log("Angka 39 ",div(39));
+console.log("Angka 76 ",div(76));
+console.log("Angka 50 ",div(50));
+console.log("Angka 9 ",div(9));
+console.log("Angka 7 ",div(7));
+console.log("Angka 41 ",div(41));
+console.log("Angka 2 ",div(2));
+console.log("Angka 24 ",div(24));
+console.log("Angka 1 ",div(1));
+console.log("Angka 16 ",div(16));
+
